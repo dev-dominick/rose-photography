115e878da5804485f9f18f7d174505bd286023ef
dominick <domalbano35@gmail.com>
2022-12-16 14:32:57 -0500

    addded about me info

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -5,7 +5,27 @@ import '../styles/aboutMe.css'
 export default function About() {
   return (
     <div className='about'>
-      <h1>About Page</h1>
+      <h1>About Me!!!!</h1>
+      <p>
+        Hi Everyone! Welcome to Rose Photography..
+        where life's precious moments are caught by a lens.
+        I am new to the photography world in that I am a full-time
+        registered nurse. Nursing is something I've loved for a very
+        long time and have been blessed to be doing it for the last 5 years.
+        I am a fur-mommy to 7 beautiful kitty cats and 4 incredible doggies
+        who all bring so much joy and love into my life. I am big on being
+        with family, constantly being with and doing things with my parents,
+        sisters, niece and nephews, and friends who are more like family.
+      </p>
+      <p>
+        I have always been passionate about photography, constantly
+        being the photographer for many family and friend events without
+        even realizing it. Now is my chance to professionally bring to life
+        people's most sacred moments. I know the importance of having
+        pictures and what they could mean for years to come. It would be
+        my absolute pleasure to capture these moments in the most fun and
+        positive way for all!
+      </p>
     </div>
   );
 }
