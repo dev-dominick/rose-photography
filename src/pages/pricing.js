808dc335655e371c529f0fe209170118fdaf7041
dominick <domalbano35@gmail.com>
2023-01-06 16:08:18 -0500

    css formatting

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -5,43 +5,27 @@ import '../styles/pricing.css'
 export default function Pricing() {
     return (
         <div className='pricing'>
-            <h1>Princing</h1>
-            <h3>
-                Services
-            </h3>
-            <div className='servicesContainer'>
+            <h1>Princing / Services</h1>
+
+            <section className='servicesContainer'>
+
                 <div className='servicesInfo'>
                     <h5> 1 Hour Shoots </h5>
-                    <p>
-                        With 15 edited photos - $150
-                    </p>
-                    <p>
-                        With 15 edited photos - $150
-                    </p>
-                    <p>
-                        With 15 edited photos - $150
-                    </p>
-                    <p>
-                        With 15 edited photos - $150
-                    </p>
+                    <p>With 15 edited photos - $150</p>
+                    <p>With 15 edited photos - $150</p>
+                    <p>With 15 edited photos - $150</p>
+                    <p>With 15 edited photos - $150</p>
                 </div>
+
                 <div className='servicesInfo'>
-                    <h5>
-                        Special Event Packages
-                        (Showers, Parties)
-
-                    </h5>
-                    <p>
-                        less than 25 miles – Full Album 75-100 photos - $350
-
-                    </p>
-                    <p>
-                        over 25 miles – Full Album 75-100 photos - $350
-
-                    </p>
+                    <h5>Special Event Packages(Showers, Parties)</h5>
+                    <p>less than 25 miles</p>
+                    <p>Full Album 75-100 photos - $350</p>
+                    <p>over 25 miles</p> 
+                    <p>Full Album 75-100 photos - $375</p>
                 </div>
 
-            </div>
+            </section>
         </div>
     )
 }
