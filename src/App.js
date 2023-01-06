e669e5e7361553e5fabecabdefcbe1c12f9d5587
dominick <domalbano35@gmail.com>
2023-01-06 16:07:11 -0500

    added gallery page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from './pages/aboutMe'
 import Gallery from './pages/gallery'
+import Gallery1 from "./pages/Gallery1";
 import Pricing from "./pages/pricing";
 import Contact from './pages/contactMe'
 import './App.css'
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={< Home />} />
           <Route path="/about" element={< About />} />
           <Route path="/gallery" element={< Gallery />} />
+          <Route path="/gallery1" element={< Gallery1 />} />
           <Route path="/pricing" element={< Pricing />} />
           <Route path="/contact" element={< Contact />} />
         </Routes>
