1dea37876fddfd01fd7501f9a9106f44bb15ad76
dominick <domalbano35@gmail.com>
2023-01-06 16:08:59 -0500

    removed theme

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,29 +5,90 @@ import Typography from "@mui/material/Typography";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import { Link } from "react-router-dom";
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import "../styles/navbar.css";
 import NavDrawer from "./Drawer";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ButtonUnstyled from "@mui/base/ButtonUnstyled";
+import { styled } from "@mui/system";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      // Purple and green play nicely together.
-      main: 'rgb(137, 138, 166)',
-    },
-    secondary: {
-      // This is green.A700 as hex.
-      main: 'rgb(201, 187, 207)',
-    },
-    accent: {
-      main: 'rgb(214, 239, 237)'
-    },
-    accentCompliment: {
-      main: 'rgb(183, 211, 223)'
-    }
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       // Purple and green play nicely together.
+//       main: 'rgb(137, 138, 166)',
+//     },
+//     secondary: {
+//       // This is green.A700 as hex.
+//       main: 'rgb(201, 187, 207)',
+//     },
+//     accent: {
+//       main: 'rgb(214, 239, 237)'
+//     },
+//     accentCompliment: {
+//       main: 'rgb(183, 211, 223)'
+//     }
+//   },
+// });
+
+const NavButton = styled(ButtonUnstyled)({
+  boxShadow: 'none',
+
+  fontFamily: 'sans-serif',
+
+  fontWeight: 'bold',
+
+  // fontSize: { xs: "", sm: "", md: "'1rem'", lg: "'1.5rem'", xl: "'3rem'", },
+
+  fontSize: '1.2rem',
+  textDecoration: 'none',
+  color: 'black',
+  transition: 'all 150ms ease',
+  cursor: 'pointer',
+  padding: ".5rem 0.5rem 0.5rem 0.5rem",
+  margin: '0.9rem 1.5rem 2rem 1.5rem',
+
+  '&:hover': {
+    textDecoration: 'underline',
+  
   },
+
+  // '&:active': {
+  //   backgroundColor: '#eeff04',
+  //   boxShadow: 'inset 0 0 13px 3px black!important',
+  //   color: 'black',
+
+  // },
+
+  // '&:active:hover': {
+  //   backgroundColor: '#eeff04',
+  //   boxShadow: 'inset 0 0 13px 3px black!important',
+  //   color: 'black',
+  // },
 });
+
+// const FullScreenHeaderSx = {
+//   margin: '.9rem 1.5rem 2rem 1.5rem',
+//   padding: '.5rem 0.5rem 0.5rem 0.5rem',
+//   display: { xs: "none", sm: "none", md: "flex", lg: "flex", xl: "flex", },
+//   paddingLeft: { sm: "none", md: "0rem", lg: "5rem", xl: "8rem" },
+//   fontFamily: "monospace",
+//   fontSize: '2rem',
+//   fontWeight: 700,
+//   letterSpacing: ".3rem",
+//   color: "white",
+//   textDecoration: 'none',
+//   // flexFlow: { xs: "", sm: "", md: "row nowrap", lg: "row nowrap", xl: "row nowrap", },
+//   // alignItems: { xs: "", sm: "", md: "center", lg: "center", xl: "center", },
+//   // height: { xs: "", sm: "", md: "5rem", lg: "5rem", xl: "5rem", },
+//   // justifyContent: { xs: "", sm: "", md: "flex-end", lg: "flex-end", xl: "flex-end", },
+//   '&:hover': {
+//     textDecoration: 'underline',
+//     textDecorationColor: '#eeff04'
+//   }
+// };
+
 
 
 
@@ -39,10 +100,10 @@ export default function Navbar() {
 
 
   return (
-    <ThemeProvider theme={theme}>
       <AppBar
-        color='primary'
+        // color='primary'
         sx={{
+          backgroundColor: 'pink !important'
         }}
       >
         <Container
@@ -67,8 +128,11 @@ export default function Navbar() {
             {/* Menu Icon for mobile */}
             < NavDrawer />
 
+            <a className="navLogoContainer" href="/">
+              <img className="navLogo" src={require('../assets/logos/logoTransparDark.png')} alt='nature'></img>
+            </a>
 
-            {/* header for full screen */}
+            {/* header for full screen
             <Typography
               variant="h5"
               noWrap
@@ -76,7 +140,7 @@ export default function Navbar() {
               href="/"
             >
               Rose Photography
-            </Typography>
+            </Typography> */}
 
             {/* mobile header: Dev Dominick */}
             <Typography
@@ -113,7 +177,7 @@ export default function Navbar() {
             >
 
               {/* About Me Button */}
-              <Button
+              <NavButton
                 color='secondary'
                 className="toUnderline"
                 key="about"
@@ -139,10 +203,10 @@ export default function Navbar() {
                 }}
               >
                 About Me
-              </Button>
+              </NavButton>
 
               {/* Gallery Button */}
-              <Button
+              <NavButton
                 color='secondary'
 
                 className="toUnderline"
@@ -169,10 +233,10 @@ export default function Navbar() {
                 }}
               >
                 Gallery
-              </Button>
+              </NavButton>
 
               {/* Pricing Button */}
-              <Button
+              <NavButton
                 color='secondary'
 
                 className="toUnderline"
@@ -199,10 +263,10 @@ export default function Navbar() {
                 }}
               >
                 Pricing
-              </Button>
+              </NavButton>
 
               {/* Contact Me Button */}
-              <Button
+              <NavButton
                 color='secondary'
 
                 className="toUnderline"
@@ -229,7 +293,7 @@ export default function Navbar() {
                 }}
               >
                 Contact Me
-              </Button>
+              </NavButton>
 
             </Box>
 
@@ -238,6 +302,6 @@ export default function Navbar() {
         </Container>
 
       </AppBar>
-    </ThemeProvider>
+    // </ThemeProvider>
   );
 }
