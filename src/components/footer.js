33f6aeed5e40b1a2897f25cbb4c741ba0e2db33a
dominick <domalbano35@gmail.com>
2023-01-14 11:22:29 -0500

    fixed footer element breaking app

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,14 +4,14 @@ import '../styles/footer.css'
 export default function Footer() {
   return (
     <div className='footer'>
-      <Footer className='footerContainer'>
+      <footer className='footerContainer'>
         <p className='footerText'>Rose Photography</p>
         <a className='footerText footerContact' href='/contact'><p>Contact me for more information</p></a>
         <p className='footerText'>Dominick Albano © 2023</p>
-      </Footer>
-      <Footer className='footerContainer'>
+      </footer>
+      {/* <Footer className='footerContainer'>
         <p>All content is property of Rose Photography</p>
-      </Footer>
+      </Footer> */}
     </div>
   )
 }
