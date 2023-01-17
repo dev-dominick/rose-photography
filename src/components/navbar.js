7ef7a506da7addbba1713af2154400ecb72f4b58
dominick <domalbano35@gmail.com>
2023-01-17 15:26:12 -0500

    font to all caps and sizeand color changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,26 +11,7 @@ import NavDrawer from "./Drawer";
 import ButtonUnstyled from "@mui/base/ButtonUnstyled";
 import { styled } from "@mui/system";
 
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       // Purple and green play nicely together.
-//       main: 'rgb(137, 138, 166)',
-//     },
-//     secondary: {
-//       // This is green.A700 as hex.
-//       main: 'rgb(201, 187, 207)',
-//     },
-//     accent: {
-//       main: 'rgb(214, 239, 237)'
-//     },
-//     accentCompliment: {
-//       main: 'rgb(183, 211, 223)'
-//     }
-//   },
-// });
 
 const NavButton = styled(ButtonUnstyled)({
   boxShadow: 'none',
@@ -38,7 +19,7 @@ const NavButton = styled(ButtonUnstyled)({
   fontFamily: 'sans-serif',
 
   fontWeight: 'bold',
-
+  // border: '1px solid red',
   // fontSize: { xs: "", sm: "", md: "'1rem'", lg: "'1.5rem'", xl: "'3rem'", },
 
   fontSize: '1.2rem',
@@ -46,12 +27,12 @@ const NavButton = styled(ButtonUnstyled)({
   color: 'black',
   transition: 'all 150ms ease',
   cursor: 'pointer',
-  padding: ".5rem 0.5rem 0.5rem 0.5rem",
-  margin: '0.9rem 1.5rem 2rem 1.5rem',
+  // padding: ".5rem 0.5rem 0.5rem 0.5rem",
+  // margin: '0.9rem 1.5rem 2rem 1.5rem',
 
   '&:hover': {
     textDecoration: 'underline',
-  
+
   },
 
   // '&:active': {
@@ -93,46 +74,68 @@ const NavButton = styled(ButtonUnstyled)({
 
 
 export default function Navbar() {
+  const [scrolled, setScrolled] = React.useState(false);
+  // const location = useLocation();
 
-
-
+  React.useEffect(() => {
+    // setting to check if script is being run in webpage
+    if (typeof window !== "undefined") {
+      window.onscroll = () => {
+        // assigning value verticle scroll to check if page has been scrolled
+        let currentScrollPos = window.pageYOffset;
+        if (currentScrollPos === 0) {
+          setScrolled(false);
+        } else {
+          setScrolled(true);
+        }
+      };
+    }
+  });
 
 
 
   return (
-      <AppBar
-        // color='primary'
-        sx={{
-          backgroundColor: 'pink !important'
-        }}
+    <AppBar
+      // color='primary'
+      sx={{
+        backgroundColor: 'white !important',
+        boxShadow: scrolled ? '"black !important"' : 'none !important',
+        height: { xs: "", sm: "", md: "", lg: "", xl: "21%", },
+        // display: 'flex', flexDirection: 'column'
+      }}
+    >
+      <Container
+        maxWidth="x1"
+        // sx={{
+        //   height: '100% !important',
+        //   // padding: "1rem",
+        //   alignContent: { xs: "", sm: "", md: "center", lg: "center", xl: "center", },
+        //   alignItems: { xs: "", sm: "", md: "center", lg: "center", xl: "center", },
+        //   flexFlow: { xs: "", sm: "", md: "row nowrap", lg: "row nowrap", xl: "row nowrap", },
+        //   height: { xs: "", sm: "", md: "5rem", lg: "5rem", xl: "5rem", },
+        //   justifyContent: { xs: "", sm: "", md: "center", lg: "center", xl: "center", },
+
+        // }}
       >
-        <Container
-          maxWidth="x1"
+        {/* app content container */}
+        <Toolbar
+          disableGutters
+          className="toolbarCon"
           sx={{
-            padding: "1rem",
-            // alignContent: { xs: "", sm: "", md: "center", lg: "center", xl: "center", },
-            // alignItems: { xs: "", sm: "", md: "center", lg: "center", xl: "center", },
-            // flexFlow: { xs: "", sm: "", md: "row nowrap", lg: "row nowrap", xl: "row nowrap", },
-            // height: { xs: "", sm: "", md: "5rem", lg: "5rem", xl: "5rem", },
-            // justifyContent: { xs: "", sm: "", md: "center", lg: "center", xl: "center", },
+            // height: '100%',
+            display: { xs: "flex", sm: "flex", md: "flex", lg: "flex", xl: "flex" },
+            flexDirection: { xs: "row-reverse", sm: "row-reverse", md: "row", lg: "column", xl: "column" },
+            alignItems: 'center'
+          }} >
 
-          }}
-        >
-          <Toolbar
-            // disableGutters
-            sx={{
-              display: { xs: "flex", sm: "flex", md: "flex", lg: "flex", xl: "flex" },
-              flexDirection: { xs: "row-reverse", sm: "row-reverse", md: "row", lg: "row", xl: "row" },
-            }} >
+          {/* Menu Icon for mobile */}
+          < NavDrawer />
 
-            {/* Menu Icon for mobile */}
-            < NavDrawer />
+          <a className="navLogoContainer" href="/">
+            <img className="navLogo" src={require('../assets/logos/logoTransparDark.png')} alt='nature'></img>
+          </a>
 
-            <a className="navLogoContainer" href="/">
-              <img className="navLogo" src={require('../assets/logos/logoTransparDark.png')} alt='nature'></img>
-            </a>
-
-            {/* header for full screen
+          {/* header for full screen
             <Typography
               variant="h5"
               noWrap
@@ -142,166 +145,195 @@ export default function Navbar() {
               Rose Photography
             </Typography> */}
 
-            {/* mobile header: Dev Dominick */}
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
+          {/* mobile header */}
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 1,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".01rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Mobile Name
+          </Typography>
+
+          {/* buttons for full screen */}
+          <Box
+            className="boxCon"
+            sx={{
+              // paddingTop: '5rem',
+              flexGrow: 1,
+              display: { xs: "none", sm: "none", md: "flex", lg: "flex", xl: "flex", },
+              // paddingRight: { sm: "none", md: "0rem", lg: "5rem", xl: "8rem" },
+              flexFlow: { xs: "", sm: "", md: "row nowrap", lg: "row nowrap", xl: "row nowrap", },
+              alignItems: { xs: "", sm: "", md: "center", lg: "center", xl: "center", },
+              // height: { xs: "", sm: "", md: "5rem", lg: "5rem", xl: "5rem", },
+              justifyContent: { xs: "", sm: "", md: "flex-end", lg: "flex-end", xl: "flex-end", },
+            }}
+          >
+
+            <NavButton
+              color='secondary'
+              className="toUnderline"
+              key="home"
+              component={Link}
+              to="/"
+              variant="contained"
               sx={{
-                mr: 1,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".01rem",
-                color: "inherit",
-                textDecoration: "none",
+                padding: {
+                  xs: "none",
+                  sm: "none",
+                  md: ".5rem 0.5rem 0.5rem 0.5rem",
+                  lg: ".5rem 0.5rem 0.5rem 0.5rem",
+                  xl: ".5rem 0.5rem 0.5rem 0.5rem",
+                },
+
+                margin: {
+                  xs: "none",
+                  sm: "none",
+                  md: '0.9rem .5rem 2rem .5rem',
+                  lg: '0.9rem 1.5rem 2rem 1.5rem',
+                  xl: '0.9rem 1.5rem 2rem 1.5rem',
+                },
               }}
             >
-              Mobile Name
-            </Typography>
+              HOME
+            </NavButton>
 
-            {/* buttons for full screen */}
-            <Box
+            {/* About Me Button */}
+            <NavButton
+              color='secondary'
+              className="toUnderline"
+              key="about"
+              component={Link}
+              to="about"
+              variant="contained"
               sx={{
-                padding: 1,
-                flexGrow: 1,
-                display: { xs: "none", sm: "none", md: "flex", lg: "flex", xl: "flex", },
-                paddingRight: { sm: "none", md: "0rem", lg: "5rem", xl: "8rem" },
-                // flexFlow: { xs: "", sm: "", md: "row nowrap", lg: "row nowrap", xl: "row nowrap", },
-                // alignItems: { xs: "", sm: "", md: "center", lg: "center", xl: "center", },
-                // height: { xs: "", sm: "", md: "5rem", lg: "5rem", xl: "5rem", },
-                justifyContent: { xs: "", sm: "", md: "flex-end", lg: "flex-end", xl: "flex-end", },
+                padding: {
+                  xs: "none",
+                  sm: "none",
+                  md: ".5rem 0.5rem 0.5rem 0.5rem",
+                  lg: ".5rem 0.5rem 0.5rem 0.5rem",
+                  xl: ".5rem 0.5rem 0.5rem 0.5rem",
+                },
+
+                margin: {
+                  xs: "none",
+                  sm: "none",
+                  md: '0.9rem .5rem 2rem .5rem',
+                  lg: '0.9rem 1.5rem 2rem 1.5rem',
+                  xl: '0.9rem 1.5rem 2rem 1.5rem',
+                },
               }}
             >
+              ABOUT ME
+            </NavButton>
 
-              {/* About Me Button */}
-              <NavButton
-                color='secondary'
-                className="toUnderline"
-                key="about"
-                component={Link}
-                to="about"
-                variant="contained"
-                sx={{
-                  padding: {
-                    xs: "none",
-                    sm: "none",
-                    md: ".5rem 0.5rem 0.5rem 0.5rem",
-                    lg: ".5rem 0.5rem 0.5rem 0.5rem",
-                    xl: ".5rem 0.5rem 0.5rem 0.5rem",
-                  },
+            {/* Gallery Button */}
+            <NavButton
+              color='secondary'
 
-                  margin: {
-                    xs: "none",
-                    sm: "none",
-                    md: '0.9rem .5rem 2rem .5rem',
-                    lg: '0.9rem 1.5rem 2rem 1.5rem',
-                    xl: '0.9rem 1.5rem 2rem 1.5rem',
-                  },
-                }}
-              >
-                About Me
-              </NavButton>
+              className="toUnderline"
+              key="gallery"
+              component={Link}
+              to="/gallery"
+              variant="contained"
+              sx={{
+                padding: {
+                  xs: "none",
+                  sm: "none",
+                  md: ".5rem 0.5rem 0.5rem 0.5rem",
+                  lg: ".5rem 0.5rem 0.5rem 0.5rem",
+                  xl: ".5rem 0.5rem 0.5rem 0.5rem",
+                },
 
-              {/* Gallery Button */}
-              <NavButton
-                color='secondary'
+                margin: {
+                  xs: "none",
+                  sm: "none",
+                  md: '0.9rem .5rem 2rem .5rem',
+                  lg: '0.9rem 1.5rem 2rem 1.5rem',
+                  xl: '0.9rem 1.5rem 2rem 1.5rem',
+                },
+              }}
+            >
+              GALLERY
+            </NavButton>
 
-                className="toUnderline"
-                key="gallery"
-                component={Link}
-                to="/gallery"
-                variant="contained"
-                sx={{
-                  padding: {
-                    xs: "none",
-                    sm: "none",
-                    md: ".5rem 0.5rem 0.5rem 0.5rem",
-                    lg: ".5rem 0.5rem 0.5rem 0.5rem",
-                    xl: ".5rem 0.5rem 0.5rem 0.5rem",
-                  },
+            {/* Pricing Button */}
+            <NavButton
+              color='secondary'
 
-                  margin: {
-                    xs: "none",
-                    sm: "none",
-                    md: '0.9rem .5rem 2rem .5rem',
-                    lg: '0.9rem 1.5rem 2rem 1.5rem',
-                    xl: '0.9rem 1.5rem 2rem 1.5rem',
-                  },
-                }}
-              >
-                Gallery
-              </NavButton>
+              className="toUnderline"
+              key="pricing"
+              component={Link}
+              to="/pricing"
+              variant="contained"
+              sx={{
+                padding: {
+                  xs: "none",
+                  sm: "none",
+                  md: ".5rem 0.5rem 0.5rem 0.5rem",
+                  lg: ".5rem 0.5rem 0.5rem 0.5rem",
+                  xl: ".5rem 0.5rem 0.5rem 0.5rem",
+                },
 
-              {/* Pricing Button */}
-              <NavButton
-                color='secondary'
+                margin: {
+                  xs: "none",
+                  sm: "none",
+                  md: '0.9rem .5rem 2rem .5rem',
+                  lg: '0.9rem 1.5rem 2rem 1.5rem',
+                  xl: '0.9rem 1.5rem 2rem 1.5rem',
+                },
+              }}
+            >
+              PRICING
+            </NavButton>
 
-                className="toUnderline"
-                key="pricing"
-                component={Link}
-                to="/pricing"
-                variant="contained"
-                sx={{
-                  padding: {
-                    xs: "none",
-                    sm: "none",
-                    md: ".5rem 0.5rem 0.5rem 0.5rem",
-                    lg: ".5rem 0.5rem 0.5rem 0.5rem",
-                    xl: ".5rem 0.5rem 0.5rem 0.5rem",
-                  },
+            {/* Contact Me Button */}
+            <NavButton
+              color='secondary'
 
-                  margin: {
-                    xs: "none",
-                    sm: "none",
-                    md: '0.9rem .5rem 2rem .5rem',
-                    lg: '0.9rem 1.5rem 2rem 1.5rem',
-                    xl: '0.9rem 1.5rem 2rem 1.5rem',
-                  },
-                }}
-              >
-                Pricing
-              </NavButton>
+              className="toUnderline"
+              key="contact"
+              component={Link}
+              to="/contact"
+              variant="contained"
+              sx={{
+                padding: {
+                  xs: "none",
+                  sm: "none",
+                  md: ".5rem 0.5rem 0.5rem 0.5rem",
+                  lg: ".5rem 0.5rem 0.5rem 0.5rem",
+                  xl: ".5rem 0.5rem 0.5rem 0.5rem",
+                },
 
-              {/* Contact Me Button */}
-              <NavButton
-                color='secondary'
+                margin: {
+                  xs: "none",
+                  sm: "none",
+                  md: '0.9rem .5rem 2rem .5rem',
+                  lg: '0.9rem 1.5rem 2rem 1.5rem',
+                  xl: '0.9rem 1.5rem 2rem 1.5rem',
+                },
+              }}
+            >
+              CONTACT ME
+            </NavButton>
 
-                className="toUnderline"
-                key="contact"
-                component={Link}
-                to="/contact"
-                variant="contained"
-                sx={{
-                  padding: {
-                    xs: "none",
-                    sm: "none",
-                    md: ".5rem 0.5rem 0.5rem 0.5rem",
-                    lg: ".5rem 0.5rem 0.5rem 0.5rem",
-                    xl: ".5rem 0.5rem 0.5rem 0.5rem",
-                  },
+          </Box>
 
-                  margin: {
-                    xs: "none",
-                    sm: "none",
-                    md: '0.9rem .5rem 2rem .5rem',
-                    lg: '0.9rem 1.5rem 2rem 1.5rem',
-                    xl: '0.9rem 1.5rem 2rem 1.5rem',
-                  },
-                }}
-              >
-                Contact Me
-              </NavButton>
+        </Toolbar>
 
-            </Box>
+      </Container>
 
-          </Toolbar>
-
-        </Container>
-
-      </AppBar>
+    </AppBar>
     // </ThemeProvider>
   );
 }
