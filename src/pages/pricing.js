4bff1a9f0a75734584808d5195a13f12b5cd1ea1
dominick <domalbano35@gmail.com>
2023-01-17 15:27:58 -0500

    overhaul on price page

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -6,24 +6,55 @@ import '../App.css'
 export default function Pricing() {
     return (
         <div className='pricing main'>
-            <h1>Princing / Services</h1>
+
+            <header className='welcomeBanner'>
+                <h1 className='bannerText'>Pricing / Services</h1>
+            </header>
 
             <section className='servicesContainer'>
 
                 <div className='servicesInfo'>
-                    <h5 className='serviceHeader'> 1 Hour Shoots </h5>
-                    <p>With 15 edited photos - $150</p>
-                    <p>With 30 edited photos - $200</p>
-                    <p>With 45 edited photos - $250</p>
-                    <p>With 60 edited photos - $300</p>
+                    <div className='serviceHeaderContainer'>
+                        <h5 className='serviceHeader'> 1 Hour Shoots </h5>
+                    </div>
+                    <div className='servicePhoto'>
+
+                    </div>
+                    <div className='pricingLayout'>
+                        <p className='servicesText'>With 15 edited photos - $150</p>
+                        <p className='servicesText'>With 30 edited photos - $200</p>
+                        <p className='servicesText'>With 45 edited photos - $250</p>
+                        <p className='servicesText'>With 60 edited photos - $300</p>
+                    </div>
+
                 </div>
 
                 <div className='servicesInfo'>
-                    <h5 className='serviceHeader'>Special Event Packages <br></br> (Showers, Parties)</h5>
-                    <p>less than 25 miles</p>
-                    <p>Full Album 75-100 photos - $350</p>
-                    <p>over 25 miles</p> 
-                    <p>Full Album 75-100 photos - $375</p>
+
+                    <div className='serviceHeaderContainer'>
+                        <h5 className='serviceHeader'>Special Event Packages</h5>
+                    </div>
+
+                    <div className='apples'>
+                        <div className='partyAndShowersHeaderContainer'>
+                            <div className='partyAndShowersHeader'>Showers</div>
+                            <div className='partyAndShowersPhoto'></div>
+                        </div>
+
+                        <div className='partyAndShowersHeaderContainer'>
+                            <div className='partyAndShowersHeader'>Parties</div>
+                            <div className='partyAndShowersPhoto'></div>
+                        </div>
+                    </div>
+
+                    <div className='pricingLayout'>
+
+                        <p className='servicesText'>less than 25 miles</p>
+                        <p className='servicesText'>Full Album 75-100 photos - $350</p>
+                        <p className='servicesText'>over 25 miles</p>
+                        <p className='servicesText'>Full Album 75-100 photos - $375</p>
+                    </div>
+
                 </div>
 
             </section>
