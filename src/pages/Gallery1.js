4eeab276e6e53fd2690ee71fa76cc8a8065910f7
dominick <domalbano35@gmail.com>
2023-01-28 14:01:54 -0500

    rendering carousel to gallery page

--- src/pages/Gallery1.js
+++ src/pages/Gallery1.js
@@ -5,18 +5,17 @@ import '../App.css'
 
 function gallery1() {
 
-const kidPics = [
-  require('../assets/ayarsKids/ayarsKids1.jpg'),
-  require('../assets/ayarsKids/ayarsKids2.jpg'),
-  require('../assets/ayarsKids/ayarsKids3.jpg'),
-  require('../assets/ayarsKids/ayarsKids4.jpg'),
-  require('../assets/ayarsKids/ayarsKids5.jpg'),
-]
+  const kidPics = [
+    { src: require('../assets/ayarsKids/ayarsKids1.jpg') },
+    { src: require('../assets/ayarsKids/ayarsKids2.jpg') },
+    { src: require('../assets/ayarsKids/ayarsKids3.jpg') },
+    { src: require('../assets/ayarsKids/ayarsKids4.jpg') },
+    { src: require('../assets/ayarsKids/ayarsKids5.jpg') },
+  ]
 
   return (
     <div className='galleryDisplay main'>
-     <Carousel slides={kidPics} /> 
-        
+      <Carousel slides={kidPics} allPhotos={kidPics}/>
     </div>
   )
 }
