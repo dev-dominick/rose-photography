785a30ce3616ebe69c819aed237de9aeffb96a56
dominick <domalbano35@gmail.com>
2023-02-03 14:37:25 -0500

    added gallery display

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,48 +5,49 @@ import '../styles/carousel.css'
 export default function Carousel({ slides, allPhotos }) {
 
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(slides[0].src);
+  // const []
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex(currentIndex => (currentIndex + 1) % slides.length);
-    }, 10000);
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentIndex(currentIndex => (currentIndex + 1) % slides.length);
+  //   }, 10000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   const handleNext = () => {
-    setCurrentIndex(currentIndex => (currentIndex + 1) % slides.length);
+    setCurrentIndex(currentIndex => (slides[0 + 1].src));
   };
   const handleBack = () => {
-    setCurrentIndex(currentIndex => (currentIndex - 1) % slides.length);
+    setCurrentIndex(currentIndex => (currentIndex - 1));
   };
+
+console.log(slides[0].src);
+
 
   return (
     <div className='carousel'>
 
-      {/* <div>
-        <img className='slideImage' src={slides[currentIndex]}></img>
+      <div className='mainImageContainer'>
+        <img className='mainImage' src={currentIndex} alt={'hi'}></img>
       </div>
 
+        {/* <button onClick={handleBack}>Back</button> */}
+        {/* <button onClick={handleNext}>Next</button> */}
 
-      <div className='photoDisplayContainer'>
-        <button onClick={handleBack}>Back</button>
-        <button onClick={handleNext}>Next</button>
-        <div>
-          <img className='slideImage' src={slides[currentIndex]}></img>
-        </div>
-      </div> */}
-
-      {allPhotos.map((p) => (
+      
+      <div className='dogs'>
+        {allPhotos.map((p, key) => (
 
 
-        <div className='hey'>
-          <div className='display'>
-            <img className='slideImage' id='src' src={p.src} alt={"hey"} />
-          </div>
-        </div>
+            <div key={key} className='display' onClick={() => setCurrentIndex(p.src) }>
+              <img className='slideImage' id='src' src={p.src} alt={"hey"} />
+            </div>
 
-      ))}
+        ))}
+
+      </div>
+
 
     </div>
   );
--- src/pages/Gallery1.js
+++ src/pages/Gallery1.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import Carousel from '../components/Carousel';
+// import Carousel from '../components/Carousel';
+import GalleryCarousel from '../components/GalleryCarousel';
 import '../styles/galleryDisplay.css'
 import '../App.css'
 
 function gallery1() {
 
-  const kidPics = [
-    { src: require('../assets/ayarsKids/ayarsKids1.jpg') },
-    { src: require('../assets/ayarsKids/ayarsKids2.jpg') },
-    { src: require('../assets/ayarsKids/ayarsKids3.jpg') },
-    { src: require('../assets/ayarsKids/ayarsKids4.jpg') },
-    { src: require('../assets/ayarsKids/ayarsKids5.jpg') },
-  ]
+  // const kidPics = [
+  //   { src: require('../assets/ayarsKids/ayarsKids1.jpg') },
+  //   { src: require('../assets/ayarsKids/ayarsKids2.jpg') },
+  //   { src: require('../assets/ayarsKids/ayarsKids3.jpg') },
+  //   { src: require('../assets/ayarsKids/ayarsKids4.jpg') },
+  //   { src: require('../assets/ayarsKids/ayarsKids5.jpg') },
+  // ]
+
+  const kidPics = [1, 2, 3, 4, 5].map((number) => ({
+    src: require(`../assets/ayarsKids/ayarsKids${number}.jpg`)
+}));
+
 
   return (
     <div className='galleryDisplay main'>
-      <Carousel slides={kidPics} allPhotos={kidPics}/>
+      {/* <Carousel slides={kidPics} allPhotos={kidPics}/> */}
+      < GalleryCarousel images={kidPics} />
     </div>
   )
 }
