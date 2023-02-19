bc141e58543d1be215c92952fff5c1ce2c7d4999
dominick <domalbano35@gmail.com>
2023-02-19 13:06:01 -0500

    removed old code from gallery page

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,56 +1,8 @@
 import React from 'react'
 import "../styles/gallery.css";
-// import Carousel from '../components/Carousel';
 import GalleryDisplay from '../components/GalleryDisplay';
 
 export default function Gallery() {
-
-  // const galleryPages = [
-  //   {
-  //     pageName: 'Precious Moments',
-  //     href: '/gallery1',
-  //     description: "All of life's precious moments captured",
-  //     src: require('../assets/ayarsKids/ayarsKids1.jpg'),
-  //     alt: 'gallery page for baby pictures'
-  //   },
-  //   {
-  //     pageName: 'Family',
-  //     href: '/gallery2',
-  //     description: 'Photos of family',
-  //     src: require('../assets/ayarsKids/ayarsKids2.jpg'),
-  //     alt: 'gallery page for baby pictures'
-  //   },
-  //   {
-  //     pageName: 'Nature',
-  //     href: '/gallery3',
-  //     description: 'nature captured',
-  //     src: require('../assets/ayarsKids/ayarsKids3.jpg'),
-  //     alt: 'gallery page for baby pictures'
-  //   },
-  //   {
-  //     pageName: 'Precious Moments',
-  //     href: '/gallery1',
-  //     description: "All of life's precious moments captured",
-  //     src: require('../assets/ayarsKids/ayarsKids1.jpg'),
-  //     alt: 'gallery page for baby pictures'
-  //   },
-  //   {
-  //     pageName: 'Family',
-  //     href: '/gallery2',
-  //     description: 'Photos of family',
-  //     src: require('../assets/ayarsKids/ayarsKids2.jpg'),
-  //     alt: 'gallery page for baby pictures'
-  //   },
-  //   {
-  //     pageName: 'Nature',
-  //     href: '/gallery3',
-  //     description: 'nature captured',
-  //     src: require('../assets/ayarsKids/ayarsKids3.jpg'),
-  //     alt: 'gallery page for baby pictures'
-  //   },
-
-  // ]
-
 
   return (
 
@@ -59,24 +11,10 @@ export default function Gallery() {
         <h1 className='bannerText'>Gallery</h1>
       </header>
       < GalleryDisplay />
-
-
     </div>
   );
 }
 
-// href={page.href}
-
-
-/* <Carousel slides={kidPics} /> */ 
-
-// const kidPics = [
-//   require('../assets/ayarsKids/ayarsKids1.jpg'),
-//   require('../assets/ayarsKids/ayarsKids2.jpg'),
-//   require('../assets/ayarsKids/ayarsKids3.jpg'),
-//   require('../assets/ayarsKids/ayarsKids4.jpg'),
-//   require('../assets/ayarsKids/ayarsKids5.jpg'),
-// ]
 
 
 
