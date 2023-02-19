d706a963ec44c71d11a92b85eade3d6eae547de3
dominick <domalbano35@gmail.com>
2023-02-19 13:05:06 -0500

    imported renamed gallery comp and set routes for possible use

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from './pages/aboutMe'
 import Gallery from './pages/gallery'
-import Gallery1 from "./pages/Gallery1";
 import Pricing from "./pages/pricing";
 import Contact from './pages/contactMe'
 import Navbar from './components/navbar';
 import Footer from './components/footer'
 import './App.css'
+
+import KidsGallery from "./GalleryPages/KidsGallery";
 
 function App() {
   return (
@@ -16,8 +17,14 @@ function App() {
       <Routes>
         <Route path="/" element={< Home />} />
         <Route path="/about" element={< About />} />
-        <Route path="/gallery" element={< Gallery />} />
-        <Route path="/gallery1" element={< Gallery1 />} />
+        <Route path="/gallery" element={< Gallery />} />        
+        <Route path="/gallery/businessgallery" element={< KidsGallery />} />
+        <Route path="/personalgallery" element={< KidsGallery />} />
+        <Route path="/showersgallery" element={< KidsGallery />} />
+        <Route path="/partiesgallery" element={< KidsGallery />} />
+        <Route path="/animalsgallery" element={< KidsGallery />} />
+        <Route path="/kidsgallery" element={< KidsGallery />} />
+        <Route path="/familiesgallery" element={< KidsGallery />} />
         <Route path="/pricing" element={< Pricing />} />
         <Route path="/contact" element={< Contact />} />
       </Routes>
