023c9d1eaa5df6ba060d2d42ab3ecf1966947461
dominick <domalbano35@gmail.com>
2023-03-26 11:49:55 -0400

    removed commented code

--- src/components/AlbumCarousel.js
+++ src/components/AlbumCarousel.js
@@ -5,28 +5,7 @@ import React from 'react';
 
 
 function AlbumCarousel({  albumImages }) {
-//   const images = [
-//     {
-//         original: require('../assets/ayarsKids/ayarsKids1.jpg'),
-//         thumbnail: require('../assets/ayarsKids/ayarsKids1.jpg'),
-//     },
-//     {
-//         original: require('../assets/ayarsKids/ayarsKids2.jpg'),
-//         thumbnail: require('../assets/ayarsKids/ayarsKids2.jpg'),
-//     },
-//     {
-//         original: require('../assets/ayarsKids/ayarsKids3.jpg'),
-//         thumbnail: require('../assets/ayarsKids/ayarsKids3.jpg'),
-//     },
-//     {
-//         original: require('../assets/ayarsKids/ayarsKids4.jpg'),
-//         thumbnail: require('../assets/ayarsKids/ayarsKids4.jpg'),
-//     },
-//     {
-//         original: require('../assets/ayarsKids/ayarsKids5.jpg'),
-//         thumbnail: require('../assets/ayarsKids/ayarsKids5.jpg'),
-//     },
-// ];
+
   return (
     <ImageGallery items={ albumImages } />
   );
