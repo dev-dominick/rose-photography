faa4db4a86decaad5fb4eb2bd656d298ba8aac57
Dom <Domalbano35@gmail.com>
2023-04-06 20:33:40 -0400

    deploy fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import { Link, useLocation } from "react-router-dom";
